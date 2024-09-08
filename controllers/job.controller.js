06ff9f033e54bd032a4e913f7a420425f20eeef4
lutforrahmanseu@gmail.com <lutforrahmanseu@gmail.com>
2024-09-08 15:39:09 +0600

    complete backend

---
+++ controllers/job.controller.js
@@ -0,0 +1,125 @@
+import { Job } from "../models/job.model.js";
+
+//admin post job
+export const postJob = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      requirements,
+      salary,
+      location,
+      jobType,
+      position,
+      experience,
+      companyId,
+    } = req.body;
+    const userId = req.id;
+
+    if (
+      !title ||
+      !description ||
+      !requirements ||
+      !salary ||
+      !location ||
+      !jobType ||
+      !experience ||
+      !position ||
+      !companyId
+    ) {
+      return res.status(400).json({
+        message: "No job found",
+        success: false,
+      });
+    }
+    const jobs = await Job.create({
+      title,
+      description,
+      requirements: requirements.split(","),
+      salary: Number(salary),
+      location,
+      jobType,
+      experienceLevel: experience,
+      position,
+      company: companyId,
+      created_by: userId,
+    });
+    return res.status(200).json({
+      message: "Jobs found successfully",
+      jobs,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//student
+export const getAllJobs = async (req, res) => {
+  try {
+    const keyword = req.query.keyword || "";
+    const query = {
+      $or: [
+        { title: { $regex: keyword, $options: "i" } },
+        { description: { $regex: keyword, $options: "i" } },
+      ],
+    };
+    const jobs = await Job.find(query).populate({
+      path: "company" }).sort({ createdAt: -1 });
+    if (!jobs) {
+      return res.status(404).json({
+        message: "No jobs found",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      message: "Jobs found successfully",
+      jobs,
+      success: true,
+    });
+  } catch (error) {}
+};
+
+export const getJobById = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId);
+    if (!job) {
+      return res.status(404).json({
+        message: "Job not found",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      message: "Job found successfully",
+      job,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//admin crete job
+
+export const getAdminJobs = async (req, res) => {
+    try {
+        const adminId = req.id;
+        const jobs = await Job.find({ created_by: adminId }).populate({
+            path:'company',
+            createdAt:-1
+        });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import connectDB from "./utils/dbconnection.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 const app = express();
 
@@ -24,6 +26,8 @@ const PORT = process.env.PORT || 5000;
 //api'connect
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT, () => {
   connectDB();
