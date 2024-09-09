b465738f880403a3b4b787b766d7363af804a229
lutforrahmanseu@gmail.com <lutforrahmanseu@gmail.com>
2024-09-09 14:51:48 +0600

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 //api'connect
 app.use("/api/v1/user", userRoute);
